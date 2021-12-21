6adca0322d973cbdbb1465e27e297a5925cb1f4b
mdukin <93154981+mdukin@users.noreply.github.com>
2021-12-21 08:48:59 +0100

    Delete routes directory

--- routes/route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/', async function(req, res, next) {
-    res.sendFile("D:/or/lab2/index.html")
-});
-
-module.exports = router;
